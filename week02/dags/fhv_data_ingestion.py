1d50791610d04d37cce91d15b562103a069328ff
shaolang <shaolang@ymail.com>
2022-02-07 16:52:47 +0800

    Use non-deprecated airflow.operators modules

--- week02/dags/fhv_data_ingestion.py
+++ week02/dags/fhv_data_ingestion.py
@@ -1,6 +1,6 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 import ingest_script as i
 
